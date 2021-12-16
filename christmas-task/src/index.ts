044d9aff0a10ce04a6798b24009392b61d8a17fc
Antonina Berchuk <tonyaberch@gmail.com>
2021-12-16 16:36:54 +0100

    refactor:  add score

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -2,3 +2,5 @@ import Application from './app/application';
 import './global.css';
 
 const app = new Application(document.body);
+
+console.log('Самооценка 218. Все пункты выполнены, кроме: при открытии приложения курсор находится в поле поиска')
